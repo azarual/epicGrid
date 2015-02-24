702cc21f03a67ee04fab5bb9b4485fbefeb29eab
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-02-24 13:10:22 +0200

    Update epicGrid.js

--- js/epicGrid.js
+++ js/epicGrid.js
@@ -1,46 +1,28 @@
 /*! epicGrid.js v0.8 | GNU License | git.io/AZCS | Paul Browne | 2015 */
 function epicGrid() {
-    for (var a = 656, b = 1024, c = 4, d = window.innerWidth, e = document.getElementsByTagName("*"), f = document.getElementsByClassName("egcf"), g = document.querySelectorAll('[class*="-col-"],[class*="-center-"]'); f.length > 0;) f[0].parentNode.removeChild(f[0]);
-    for (var h = 0; h < g.length; ++h) {
-        var i = g[h].style;
-        i.float = "none", i.width = "100%", i.marginLeft = 0, i.marginRight = 0, i.position = "relative", i.display = "block"
+    for (var a = window.innerWidth, b = document.getElementsByTagName("*"), c = document.getElementsByClassName("egcf"), d = document.querySelectorAll('[class*="-col-"],[class*="-center-"]'); 0 < c.length;) c[0].parentNode.removeChild(c[0]);
+    for (c = 0; c < d.length; ++c) {
+        var e = d[c].style;
+        e["float"] = "none", e.width = "100%", e.marginLeft = 0, e.marginRight = 0, e.position = "relative", e.display = "block"
     }
-    if (a >= d) var j = "m";
-    if (b >= d && d > a) var j = "t";
-    if (d > b) var j = "d";
-    for (var k = 0; k < e.length; ++k)
-        for (var l = e[k].children, m = 0, n = 0; n < l.length; ++n) {
-            var o = new RegExp(j + "-col-\\d+-\\d+", "g"),
-                p = new RegExp(j + "-center-\\d+-\\d+", "g"),
-                q = new RegExp(j + "-left-\\d+-\\d+", "g"),
-                r = new RegExp(j + "-right-\\d+-\\d+", "g"),
-                s = new RegExp(j + "-offset-\\d+-\\d+", "g"),
-                t = new RegExp(j + "-col-\\d+-\\d+|" + j + "-offset-\\d+-\\d+", "g"),
-                u = new RegExp(j + "-col-\\d+-\\d+|" + j + "-center-\\d+-\\d+", "g"),
-                v = new RegExp(j + "-left-\\d+-\\d+|" + j + "-right-\\d+-\\d+", "g");
-            if (o.test(l[n].className) && !q.test(l[n].className) && !r.test(l[n].className)) {
-                var w = l[n].className.match(t).toString().match(/\d+/g);
-                if (Math.round(1e4 * m) / 1e4 >= 1 && (m += -1), 0 == Math.round(1e4 * m) / 1e4 && (l[n].className = l[n].className + " eg-first"), m += w.length > 2 ? w[0] / w[1] + w[2] / w[3] : w[0] / w[1], 0 == Math.round(1e4 * m) / 1e4 % 1) {
-                    var x = document.createElement("div");
-                    x.style.clear = "both", x.className = "egcf", l[n].parentNode.insertBefore(x, l[n].nextSibling)
-                }
-            }
-            if (u.test(l[n].className)) {
-                var y = l[n].className.match(u).toString().match(/\d+/g),
-                    z = y[0] / y[1] * (c + 100 - c * y[1]) + c * y[0] - c;
-                if (l[n].style.width = z + "%", l[n].style.marginLeft = c + "%", l[n].style.float = "left", l[n].className.match(/eg\-first/g) && (l[n].style.marginLeft = 0), l[n].className.match(p) && (l[n].style.float = "none", l[n].style.marginLeft = "auto", l[n].style.marginRight = "auto"), l[n].className.match(s)) {
-                    var A = l[n].className.match(s).toString().match(/\d+/g),
-                        B = A[0] / A[1] * (c + 100 - c * A[1]) + c * A[0] - c;
-                    l[n].style.marginLeft = B + 2 * c + "%", l[n].className.match(/eg\-first/g) && (l[n].style.marginLeft = B + c + "%")
-                }
-                if (v.test(l[n].className)) {
-                    var C = l[n].className.match(v).toString().match(/\d+/g),
-                        D = C[0] / C[1] * (c + 100 - c * C[1]) + c * C[0] - c,
-                        E = parseFloat(l[n].parentNode.style.width);
-                    l[n].className.match(q) && (l[n].style.marginLeft = 100 / E * -(D + c) + "%", l[n].style.marginRight = c + "%", l[n].style.float = "left"), l[n].className.match(r) && (l[n].style.float = "right", l[n].style.marginRight = 100 / E * -(D + c) + "%", l[n].style.marginLeft = c + "%")
-                }
-                for (var F = document.getElementsByClassName("eg-first"), G = 0; G < F.length; ++G) F[0].className = F[0].className.replace(/\seg-first/, "")
-            }
+    for (d = 0; d < b.length; ++d)
+        for (var c = b[d].children, f = e = 0; f < c.length; ++f) {
+            var g = RegExp("h-col-|h-center-", "g"),
+                h = RegExp("l-col-|l-center-", "g"),
+                i = RegExp("m-col-|m-center-", "g"),
+                j = RegExp("s-col-|s-center-", "g"),
+                k = "x";
+            a > 320 && j.test(c[f].className) && (k = "s"), a > 656 && i.test(c[f].className) && (k = "m"), a > 1024 && h.test(c[f].className) && (k = "l"), a > 1440 && g.test(c[f].className) && (k = "h");
+            var j = new RegExp(k + "-center-\\d+-\\d+", "g"),
+                g = new RegExp(k + "-left-\\d+-\\d+", "g"),
+                h = new RegExp(k + "-right-\\d+-\\d+", "g"),
+                l = new RegExp(k + "-offset-\\d+-\\d+", "g"),
+                m = new RegExp(k + "-col-\\d+-\\d+|" + k + "-offset-\\d+-\\d+", "g"),
+                n = new RegExp(k + "-col-\\d+-\\d+|" + k + "-center-\\d+-\\d+", "g"),
+                i = new RegExp(k + "-left-\\d+-\\d+|" + k + "-right-\\d+-\\d+", "g");
+            if (!new RegExp(k + "-col-\\d+-\\d+", "g").test(c[f].className) || g.test(c[f].className) || h.test(c[f].className) || (k = c[f].className.match(m).toString().match(/\d+/g), 1 <= Math.round(1e4 * e) / 1e4 && (e += -1), 0 == Math.round(1e4 * e) / 1e4 && (c[f].className += " eg-first"), e = 2 < k.length ? e + (k[0] / k[1] + k[2] / k[3]) : e + k[0] / k[1], 0 == Math.round(1e4 * e) / 1e4 % 1 && (k = document.createElement("div"), k.style.clear = "both", k.className = "egcf", c[f].parentNode.insertBefore(k, c[f].nextSibling))), n.test(c[f].className))
+                for (k = c[f].className.match(n).toString().match(/\d+/g), c[f].style.width = k[0] / k[1] * (104 - 4 * k[1]) + 4 * k[0] - 4 + "%", c[f].style.marginLeft = "4%", c[f].style["float"] = "left", c[f].className.match(/eg\-first/g) && (c[f].style.marginLeft = 0), c[f].className.match(j) && (c[f].style["float"] = "none", c[f].style.marginLeft = "auto", c[f].style.marginRight = "auto"), c[f].className.match(l) && (k = c[f].className.match(l).toString().match(/\d+/g), k = k[0] / k[1] * (104 - 4 * k[1]) + 4 * k[0] - 4, c[f].style.marginLeft = k + 8 + "%", c[f].className.match(/eg\-first/g) && (c[f].style.marginLeft = k + 4 + "%")), i.test(c[f].className) && (i = c[f].className.match(i).toString().match(/\d+/g), i = i[0] / i[1] * (104 - 4 * i[1]) + 4 * i[0] - 4, k = parseFloat(c[f].parentNode.style.width), c[f].className.match(g) && (c[f].style.marginLeft = 100 / k * -(i + 4) + "%", c[f].style.marginRight = "4%", c[f].style["float"] = "left"), c[f].className.match(h) && (c[f].style["float"] = "right", c[f].style.marginRight = 100 / k * -(i + 4) + "%", c[f].style.marginLeft = "4%")), g = document.getElementsByClassName("eg-first"), h = 0; h < g.length; ++h) g[0].className = g[0].className.replace(/\seg-first/, "")
         }
+    a = new CustomEvent("eg-ready"), document.dispatchEvent(a)
 }
 window.addEventListener("resize", epicGrid), epicGrid();
